a2c95d5e83572e0e9176c146bc214a86b11a5034
Will Santos <willsantos.edf@gmail.com>
2022-10-22 23:47:15 -0300

    Corrigindo problemas de declaração de variáveis nas colunas do banco de dados

--- receita/models.py
+++ receita/models.py
@@ -6,9 +6,9 @@ from datetime import datetime
 
 class Receita(models.Model):
     nome_receita = models.CharField(max_length=200)
-    ingredientes = models.TextField()
+    ingrediente = models.TextField()
     modo_preparo = models.TextField()
     tempo_preparo = models.IntegerField()
     rendimento = models.CharField(max_length=100)
     categoria = models.CharField(max_length=100)
-    date_receita = models.DateTimeField(default=datetime.now, blank=True)
+    data_receita = models.DateTimeField(default=datetime.now, blank=True)
--- receita/views.py
+++ receita/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from .models import Receita
 
+
 def index(request):
     receitas = Receita.objects.all()
 
@@ -8,6 +9,7 @@ def index(request):
         'receitas' : receitas
     }
     return render(request,'index.html', dados)
+
 
 def receita(request, receita_id):
     receita = get_object_or_404(Receita, pk=receita_id)
